ec531937324b844711daa4b2fc589dd3f5889ec1
ala eddine <alaeddine.elmokhtar@gmail.com>
2023-10-23 10:10:16 +0200

    react  props

---
+++ src/composant/players.js
@@ -0,0 +1,20 @@
+import ronaldo from "./img/ronaldo.png";
+import saad from "./img/saad.png"
+import ozil from "./img/ozil.png"
+const players = [
+    {name:"Ronaldo",
+    team:"Nasr", nationality:"portugal",jerseyNumber:7,age:38,
+    src:ronaldo},
+
+    {name:"Saad",
+    team:"Abha", nationality:"Tunisian",jerseyNumber:10,age:29,
+    src:saad},
+
+    {name:"Mané",
+    team:"Nasr", nationality:"Senegal",jerseyNumber:30,age:31,
+    src:"https://assets.bundesliga.com/player/dfl-obj-j0117h-dfl-clu-00000g-dfl-sea-0001k7.png?fit=320,320"},
+
+    {name:"Ozil",
+    team:"Arsenal", nationality:"Turky",jerseyNumber:10 ,age:35,
+    src:ozil}];
+    export default players;
